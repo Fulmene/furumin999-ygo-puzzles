f6cc5fdbc1ef29bdb36a77683b6fe6897013dc69
Ada Joule <ada.fulmina@gmail.com>
2018-08-26 02:08:54 +0700

    Refine JudgmentDay.lua

--- JudgmentDay.lua
+++ JudgmentDay.lua
@@ -17,93 +17,93 @@ local hint = { "Win this turn!" }
 -- Pendulums are PZone 0 and 1
 -- (MR4) Extra monster zones are MZone 5 and 6
 local oppSpellTrap = {
-    {30834988, POS_FACEUP, 2},            -- All-Out Attacks
-    {44095762, POS_FACEDOWN, 1},          -- Mirror Force
+    { 30834988, POS_FACEUP, 2 }, -- All-Out Attacks
+    { 44095762, POS_FACEDOWN, 1 }, -- Mirror Force
 }
 local oppPendulum = {
-    {37991342, POS_FACEUP, 1}             -- Qliphort Genome
+    { 37991342, POS_FACEUP, 1 }  -- Qliphort Genome
 }
 local oppMonster = {
-    -- Apoqliphort Killer (will be added manually below)
+    { 27279764, POS_FACEUP_ATTACK, 2 }  -- Apoqliphort Killer (will be added manually below)
 }
 local ownMonster = {
-    {37445295, POS_FACEDOWN_DEFENSE, 2}   -- Shaddoll Falco
+    { 37445295, POS_FACEDOWN_DEFENSE, 2 }  -- Shaddoll Falco
 }
 local ownPendulum = {}
 local ownSpellTrap = {
-    {04904633, POS_FACEDOWN, 1},          -- Shaddoll Core
-    {77505534, POS_FACEDOWN, 2}           -- Sinister Shadow Games
+    { 04904633, POS_FACEDOWN, 1 }, -- Shaddoll Core
+    { 77505534, POS_FACEDOWN, 2 }  -- Sinister Shadow Games
 }
 
 -- Other zones
 -- Use only the card id to add
 local oppHand = {
-    19665973      -- Battle Fader
+    19665973  -- Battle Fader
 }
 local oppMainDeck = {
-    40061558,     -- Apoqliphort Kernel
-    65518099      -- Qliphort Tool
+    40061558, -- Apoqliphort Kernel
+    65518099  -- Qliphort Tool
 }
 local oppExtraDeck = {}
 local oppFaceUpExtraDeck = {
-    91907707,     -- Qliphort Archive
-    87588741,     -- Qliphort Access
-    64496451,     -- Qliphort Disk
-    51194046      -- Qliphort Assembla
+    91907707, -- Qliphort Archive
+    87588741, -- Qliphort Access
+    64496451, -- Qliphort Disk
+    51194046  -- Qliphort Assembla
 }
 local oppGraveyard = {
-    90885155,     -- Qliphort Shell
-    13073850      -- Qliphort Alias
+    90885155, -- Qliphort Shell
+    13073850  -- Qliphort Alias
 }
 local oppBanished = {}
 
 local ownBanished = {}
 local ownGraveyard = {
-    49885567,     -- Cannahawk
-    25857246,     -- Necloth of Valkyrus
-    44394295,     -- Shaddoll Fusion
-    88240999,     -- Necloth of Decisive Arms
-    20773176,     -- Sephiraexa
-    95401059      -- Sephiracore
+    49885567, -- Cannahawk
+    25857246, -- Necloth of Valkyrus
+    44394295, -- Shaddoll Fusion
+    88240999, -- Necloth of Decisive Arms
+    20773176, -- Sephiraexa
+    95401059  -- Sephiracore
 }
 local ownFaceUpExtraDeck = {
-    58990362,     -- Sephirashiugo
-    22617205      -- Beelsephira
+    58990362, -- Sephirashiugo
+    22617205  -- Beelsephira
 }
 local ownExtraDeck = {
-    94977269,     -- Midrash
-    48424886,     -- Egrystal
-    74009824,     -- Wendigo
-    19261966,     -- Anomalilith
-    56638325,     -- Deltatheros
-    18326736,     -- Ptolemaeus
-    26329679,     -- Sacred Omega
-    73964868,     -- Sacred Pleiades
-    19048328,     -- Chouhou
-    83755611,     -- Shofuku
-    43202238,     -- Gaiza
-    56655675,     -- Ulti-Gaiapelio
-    86274272,     -- Ulti-Apelio
-    48063985,     -- Ulti-Cannahawk
-    12678870      -- Ulti-Pettlephin
+    94977269, -- Midrash
+    48424886, -- Egrystal
+    74009824, -- Wendigo
+    19261966, -- Anomalilith
+    56638325, -- Deltatheros
+    18326736, -- Ptolemaeus
+    26329679, -- Sacred Omega
+    73964868, -- Sacred Pleiades
+    19048328, -- Chouhou
+    83755611, -- Shofuku
+    43202238, -- Gaiza
+    56655675, -- Ulti-Gaiapelio
+    86274272, -- Ulti-Apelio
+    48063985, -- Ulti-Cannahawk
+    12678870  -- Ulti-Pettlephin
 }
 local ownMainDeck = {
-    21495657,     -- Sephirafuushi
-    23166823,     -- Sephirawendi
-    58016954,     -- Sephiranaga
-    43434803,     -- The Shallow Grave
-    37445295,     -- Shaddoll Falco
-    13890468,     -- Pettlephin
-    32354768,     -- Oracle of Sephira
-    89463537      -- Necloth of Unicore
+    21495657, -- Sephirafuushi
+    23166823, -- Sephirawendi
+    58016954, -- Sephiranaga
+    43434803, -- The Shallow Grave
+    37445295, -- Shaddoll Falco
+    13890468, -- Pettlephin
+    32354768, -- Oracle of Sephira
+    89463537  -- Necloth of Unicore
 }
 local ownHand = {
-    96223501,     -- Sephirathuban
-    84388461,     -- Sephirasaber
-    97211663,     -- Necloth Illusion
-    52068432,     -- Necloth of Trishula
-    52846880,     -- Necloth of Catastor
-    57777714      -- Sephirampilica
+    96223501, -- Sephirathuban
+    84388461, -- Sephirasaber
+    97211663, -- Necloth Illusion
+    52068432, -- Necloth of Trishula
+    52846880, -- Necloth of Catastor
+    57777714  -- Sephirampilica
 }
 
 -- End template --
@@ -136,7 +136,7 @@ for i, f in pairs(fields) do
         local id = c[1]
         local position = c[2]
         local zonenumber = c[3]
-        Debug.AddCard(id, owner, owner, place, zonenumber, position)
+        Debug.AddCard(id, owner, owner, place, zonenumber, position, true)
     end
 end
 
@@ -167,41 +167,7 @@ end
 
 -- Special Effects
 
--- Apoqliphort Killer
-local apoqliphortKiller = Debug.AddCard(27279764, OPPONENT, OPPONENT, LOCATION_MZONE, 2, POS_FACEUP_ATTACK)
-
-local immunity = Effect.CreateEffect(apoqliphortKiller)
-immunity:SetType(EFFECT_TYPE_SINGLE)
-immunity:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_UNCOPYABLE)
-immunity:SetRange(LOCATION_MZONE)
-immunity:SetCode(EFFECT_IMMUNE_EFFECT)
-immunity:SetValue(
-function(e, te)
-    if te:IsActiveType(TYPE_SPELL+TYPE_TRAP) then
-        return true
-    else
-        return aux.qlifilter(e, te)
-    end
-end
-)
-apoqliphortKiller:RegisterEffect(immunity)
-
-local atkdown = Effect.CreateEffect(apoqliphortKiller)
-atkdown:SetType(EFFECT_TYPE_FIELD)
-atkdown:SetCode(EFFECT_UPDATE_ATTACK)
-atkdown:SetRange(LOCATION_MZONE)
-atkdown:SetTargetRange(LOCATION_MZONE, LOCATION_MZONE)
-atkdown:SetTarget(
-function(e, c)
-    return c == apoqliphortKiller and bit.band(c:GetSummonType(), SUMMON_TYPE_SPECIAL) == SUMMON_TYPE_SPECIAL
-end
-)
-atkdown:SetValue(500)
-apoqliphortKiller:RegisterEffect(atkdown)
-
-local defdown = atkdown:Clone()
-defdown:SetCode(EFFECT_UPDATE_DEFENCE)
-apoqliphortKiller:RegisterEffect(defdown)
+-- End Special Effects
 
 Debug.ReloadFieldEnd()
 
